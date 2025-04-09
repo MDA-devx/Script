87efe48b539b88b6be118004fb97f10a477a98af
MDA <polakenfold@gmail.com>
2025-04-09 18:12:00 -0300

    more upd to qmr map

--- files-transform/ map4qmr.cpp
+++ files-transform/ map4qmr.cpp
@@ -7,18 +7,18 @@ after Rule number
         2 = Pitch Bend
         3 = Program Change
     chan: min - max (0 - 15) - out offset
-    $type: min - max  (0 - 127) - out offset
-        - For Note (0): Represents the note number.
-        - For Controller (1): Represents the CC number.
+    note/cc num/pb null/prog ch val!!!!!!!!: in min - max  (0 - 127)[trim value outside range] - out offset/fixed.
+        - For Note (0): Represents the note number. out as placeholder.
+        - For Controller (1): Represents the CC number. out as placeholder.
         - For Pitch Bend (2): Obsolete (not used).
         - For Program Change (3): Represents the offset, reverse, or fixed value.
-    value: min - max (0 - 127) - out offset min - max
+    velocity/cc value/PB rangr: min - max (0 - 127) - out offset min - max.
         - For Note (0): Represents the velocity.
-        - For Controller (1): Represents the CC value.
-        - For Pitch Bend (2): Represents the pitch bend range and scales the value.
+        - For Controller (1): Represents the CC value. out scales the value inbetween out range.
+        - For Pitch Bend (2): Represents the pitch bend range. out scales the value out range.
         - For Program Change (3): Obsolete (not used).
     placeholders:
-        1st: channel behavior (0 = offset, 1 = reverse offset, 2 = fixed value)
-        2nd: value behavior (0 = offset, 1 = reverse offset, 2 = fixed value)
-    out port: (0/1) - single value
+        1st: channel behavior (0 = offset, 1 = reverse offset, 2 = fixed value).
+        2nd: value behavior (0 = offset, 1 = reverse offset, 2 = fixed value).
+    out port: (0/1) - single value.
 */
